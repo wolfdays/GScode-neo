85094a3ca0493433478d21e9b8f7136734845cb8
wolfdays <wolfdaymax@gmail.com>
2025-01-05 17:45:02 +0100

    initial setup

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+    return {
+      passthroughFileCopy: true,
+      dir: {
+        input: "src",
+        output: "public",
+        includes: "_includes",
+      },
+    };
+  };
